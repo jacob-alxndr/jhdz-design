7b3506ca90da6e2c10758ee3813700c672e084c4
Jacob Martinez <jacobmartinez@Jacobs-MacBook-Pro.local>
2023-07-02 14:29:38 -0700

    feat: react player

--- components/VideoPlayer/index.js
+++ components/VideoPlayer/index.js
@@ -4,17 +4,18 @@ import { StructuredText } from "react-datocms";
 import clsx from "clsx";
 import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
-
+import { useStore } from "@lib/store";
 const VideoPlayer = (props) => {
   const { url, componentPadding, caption } = props;
-  // const [videoUrl, setVideoUrl] = useState(url || null);
-  // useEffect(() => {
-  //   setVideoUrl(url);
-  // }, []);
-
+  const isTouch = useStore(({ isTouch }) => isTouch);
+  const [isMobile, setIsMobile] = useState(true);
+  useEffect(() => {
+    isTouch ? setIsMobile(false) : setIsMobile(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isTouch]);
   return (
     <div className={clsx(styles.container, `padding-x-sm`, ...(componentPadding && componentPadding))}>
-      {url && <ReactPlayer url={url} loop playing muted style={{ width: "100%" }} />}
+      {url && <ReactPlayer url={url} loop playing={isMobile} muted style={{ width: "100%" }} />}
 
       {/* {caption && (
         <div className={styles.caption}>
