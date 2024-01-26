54cad17b3fa05ffaffba38edea09b22017269deb
Jacob Martinez <jacobmartinez@Jacobs-MBP.attlocal.net>
2024-01-25 16:25:16 -0800

    testing

--- pages/api/preview-end.js
+++ pages/api/preview-end.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   res.clearPreviewData({});
-  // res.statusCode = 307;
-  // res.redirect("/");
-  // res.setHeader("Location", `${req.query.redirect}`);
+  res.statusCode = 307;
+  res.redirect("/");
+  res.setHeader("Location", `${req.query.redirect}`);
   res.end();
 }
